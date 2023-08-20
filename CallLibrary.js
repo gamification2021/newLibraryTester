5c87dd751704181ec5419e2391bea6473a42bfba
akshay.kumar <gamificationhub@gmail.com>
2023-08-20 14:29:48 +0530

    omantel sample integration

--- CallLibrary.js
+++ CallLibrary.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 import { View } from 'react-native'
 import { NavPath } from 'rn-test-library-sixdee'
 // import { FullScreenTest } from 'rn-test-library-sixdee'
+import { LoadGamification, App, Gamification } from 'sample-omantel'
 
 const CallLibrary = () => {
     return (
-        <NavPath
-            callBack={(gift) => {
-                console.log('This is printed from the parent app using callback ==> Game finished,, received ' + gift);
-            }}
-        />
+        // <NavPath
+        //     callBack={(gift) => {
+        //         console.log('This is printed from the parent app using callback ==> Game finished,, received ' + gift);
+        //     }}
+        // />
         // <FullScreenTest />
         // <View />
+        // <TouchableOpacity style={{ backgroundColor: 'red', height: 200, width: 300 }} onPress={() => {
+
+        // }} />
+        // <View>
+
+        // </View>
+        <App />
     )
 }
 
